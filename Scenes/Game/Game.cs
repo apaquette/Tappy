9224be199595ee0a598284209a986fb0cd0c182f
Alex Paquette <alexandre.d.paquette@gmail.com>
2026-08-23 20:44:56 -0300

    Implement pipes spawn, scroll, and despawn

--- Scenes/Game/Game.cs
+++ Scenes/Game/Game.cs
@@ -3,13 +3,26 @@ using System;
 
 public partial class Game : Node
 {
+	[Export] private PackedScene _pipesScene;
+	[Export] private Timer _spawnTimer;
+	[Export] private Node _pipesHolder;
+	[Export] private Marker2D _upperSpawn;
+	[Export] private Marker2D _lowerSpawn;
+
+
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_spawnTimer.Timeout += SpawnPipes;
+		SpawnPipes();
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	private void SpawnPipes()
 	{
+		var pipes = _pipesScene.Instantiate<Pipes>();
+		float yPos = (float)GD.RandRange(_upperSpawn.Position.Y, _lowerSpawn.Position.Y);
+		pipes.Position = new Vector2(_upperSpawn.Position.X, yPos);
+		_pipesHolder.AddChild(pipes);
 	}
 }
