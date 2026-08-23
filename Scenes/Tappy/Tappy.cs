1b0e697f4b0b13dcce15902b355a79c15424772e
Alex Paquette <alexandre.d.paquette@gmail.com>
2026-08-23 13:55:30 -0300

    Implement world barriers and die

--- Scenes/Tappy/Tappy.cs
+++ Scenes/Tappy/Tappy.cs
@@ -4,6 +4,9 @@ using System;
 public partial class Tappy : CharacterBody2D
 {
 	const float JUMP_POWER = -350.0f;
+
+	[Export] private AnimatedSprite2D _animatedSprite2D;
+
 	private bool _jumped = false;
 	private float _gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
@@ -16,6 +19,7 @@ public partial class Tappy : CharacterBody2D
 	{
 	}
 
+
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
@@ -23,7 +27,17 @@ public partial class Tappy : CharacterBody2D
 		Velocity = velocity;
 
 		MoveAndSlide();
+
+		if(IsOnFloor())
+		{
+			Die();
+		}
 	}
 
+	private void Die()
+	{
+		SetPhysicsProcess(false);
+		_animatedSprite2D.Stop();
+	}
 
 }
