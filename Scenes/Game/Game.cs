3e21f7b2492e000635db20080e524bf23c88f222
Alex Paquette <alexandre.d.paquette@gmail.com>
2026-08-25 21:03:55 -0300

    refactor

--- Scenes/Game/Game.cs
+++ Scenes/Game/Game.cs
@@ -6,8 +6,7 @@ public partial class Game : Node
 	[Export] private PackedScene _pipesScene;
 	[Export] private Timer _spawnTimer;
 	[Export] private Node _pipesHolder;
-	[Export] private Marker2D _upperSpawn;
-	[Export] private Marker2D _lowerSpawn;
+	[Export] private Marker2D[] _spawns;
 
 	
 
@@ -29,8 +28,8 @@ public partial class Game : Node
 	private void SpawnPipes()
 	{
 		var pipes = _pipesScene.Instantiate<Pipes>();
-		float yPos = (float)GD.RandRange(_upperSpawn.Position.Y, _lowerSpawn.Position.Y);
-		pipes.Position = new Vector2(_upperSpawn.Position.X, yPos);
+		float yPos = (float)GD.RandRange(_spawns[0].Position.Y, _spawns[1].Position.Y);
+		pipes.Position = new Vector2(_spawns[0].Position.X, yPos);
 		_pipesHolder.AddChild(pipes);
 	}
 }
